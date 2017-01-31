d9542241568448c03301a2cf0308db5e6f230598
Dina Holzapfel <dinaholzapfel@dinas-macbook.home>
2017-01-31 16:01:28 +0100

    image size reduced

--- js/layout.js
+++ js/layout.js
@@ -1,11 +1,6 @@
-var pages = 6;
+var pages = 7;
 var currentpage = 1;
-for portfolio_item in site.portfolio {
-  currentpage = portfolio_item.number;
-}
-var currentdiv = document.querySelector("div.imgfull-box")
-
-console.log(currentpage);
+if (document.location.hash) { currentpage = parseInt(document.location.hash.replace('#', '')); }
 
 var nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
 var prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
@@ -13,63 +8,31 @@ var prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
 var animatingup = false;
 var animatingdown = false;
 
-function scrolltocurrent() {
-	var p2 = $( "#page"+(currentpage) );
-	var pageheight = currentdiv.position().top;
-	$('html, body').animate({ scrollTop: pageheight }, 200);
+jQuery(window).scroll(function(event) {
+
+if (animatingup==true) { return; }
+if (animatingdown==true) { return; }
+nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
+prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
+
+if (animatingup == false) {
+    if (jQuery(window).scrollTop()+jQuery(window).height()>=jQuery("#page"+(nextpage)).offset().top+50) {
+        if (nextpage > currentpage) {
+            var p2 = jQuery( "#page"+(nextpage) );
+            var pageheight = p2.position().top;
+            animatingdown = true;
+            jQuery('html, body').animate({ scrollTop: pageheight }, 800, function() { currentpage = nextpage; animatingdown = false; document.location.hash = currentpage;});
+            return;
+        }
+    }
 }
 
-function resizeDiv() {
-	vpw = $(window).width();
-	vph = $(window).height();
-	$('.imgfull-box').css({'min-height': vph + 'px'});
-}
-
-$(document).ready(function() {
-	resizeDiv();
-	//scrolltocurrent();
-});
-
-window.onresize = function(event) {
-	resizeDiv();
-	scrolltocurrent();
-}
-
-$(window).scroll(function(event) {
-
-	if (animatingup==true) { console.log("animating up..."); return; }
-	if (animatingdown==true) { console.log("animating down..."); return; }
-
-	nextpage = currentpage + 1; if (nextpage > pages) { nextpage = pages; }
-	prevpage = currentpage - 1; if (prevpage < 1) { prevpage = 1; }
-
-	console.log("scroll happened, previous page is " + prevpage + ", current page is " + currentpage + ", next page is " + nextpage);
-
-	//console.log($("#page"+(currentpage)).offset().top + " < " + $(window).scrollTop());
-
-	//console.log($(window).scrollTop()+$(window).height() + " < " + $("#page"+(nextpage)).offset().top);
-
-	if (animatingup == false) {
-		if ($(window).scrollTop()+$(window).height()>=$("#page"+(nextpage)).offset().top+50) {
-			if (nextpage > currentpage) {
-				var p2 = $( "#page"+(nextpage) );
-				var pageheight = currentdiv.position().top;
-				animatingdown = true;
-				$('html, body').animate({ scrollTop: pageheight }, 500, function() { currentpage = nextpage; animatingdown = false; document.location.hash = currentpage;});
-				return;
-			}
-		}
-	}
-
-	if (animatingdown == false) {
-		if ($(window).scrollTop()<=$("#page"+(currentpage)).offset().top-50) {
-			if (prevpage < currentpage) {
-				var p2 = $( "#page"+(currentpage) );
-				var pageheight = currentdiv.position().top-$(window).height();
-				animatingup = true;
-				$('html, body').animate({ scrollTop: pageheight }, 500, function() { currentpage = prevpage; animatingup = false; document.location.hash = currentpage;});
-				return;
-			}
-		}
-	}
-});
+if (animatingdown == false) {
+    if (jQuery(window).scrollTop()<=jQuery("#page"+(currentpage)).offset().top-50) {
+        if (prevpage < currentpage) {
+            var p2 = jQuery( "#page"+(currentpage) );
+            var pageheight = p2.position().top-jQuery(window).height();
+            animatingup = true;
+            jQuery('html, body').animate({ scrollTop: pageheight }, 800, function() { currentpage = prevpage; animatingup = false; document.location.hash = currentpage;});
+            return;
+        }
